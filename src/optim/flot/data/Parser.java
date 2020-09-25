b00ddf86f5db34a7a34ab4ff32ddf962f6cbd4ac
Rynhho <rynhho@gmail.com>
2020-09-26 01:55:35 +0200

    Added empty files, package structures

---
+++ src/optim/flot/data/Parser.java
@@ -0,0 +1,13 @@
+
+/**
+ * Class that manages communication between disk and application.
+ */
+public class Parser {
+    // Instance readInstance(String filepath);
+
+    // Solution readSolution(String filepath);
+
+    // void saveInstance(String filepath, Instance instance);
+
+    // void saveSolution(String filepath, Solution solution);
+}
