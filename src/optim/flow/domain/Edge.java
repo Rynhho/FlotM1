c351bbde29f4640a047c23bac7f3bbc63e7b113b
Rynhho <rynhho@gmail.com>
2020-11-08 17:29:45 +0100

    Forgot some members

--- src/optim/flow/domain/Edge.java
+++ src/optim/flow/domain/Edge.java
@@ -1,12 +1,26 @@
 package optim.flow.domain;
 
 public class Edge {
+	private final int source;
+	private final int destination;
+
 	private final double cost;
 	private final double capacity;
 
-	public Edge(double cost, double capacity) {
+	public Edge(int source, int destination, double cost, double capacity) {
+		this.source = source;
+		this.destination = destination;
+
 		this.cost = cost;
 		this.capacity = capacity;
+	}
+
+	public int getSource() {
+		return this.source;
+	}
+
+	public int getDestionation() {
+		return this.destination;
 	}
 
 	public double getCapacity() {
