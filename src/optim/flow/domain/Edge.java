e00efc384d8b24c12c0152cffc608d5888c9e90a
Rynhho <rynhho@gmail.com>
2020-11-08 15:34:56 +0100

    cleaner code, up to date gitignore

--- src/optim/flow/domain/Edge.java
+++ src/optim/flow/domain/Edge.java
@@ -1,22 +1,18 @@
 package optim.flow.domain;
 
-import java.util.ArrayList;
-
 public class Edge {
-
 	private final double cost;
-	
-	private double capacity;
-	
-	public Edge(double cost,double capacity) {
-		this.cost=cost;
-		this.capacity=capacity;
+	private final double capacity;
+
+	public Edge(double cost, double capacity) {
+		this.cost = cost;
+		this.capacity = capacity;
 	}
 
 	public double getCapacity() {
 		return capacity;
 	}
-	
+
 	public double getCost() {
 		return cost;
 	}
