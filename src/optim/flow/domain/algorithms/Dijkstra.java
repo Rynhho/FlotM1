22a46e849184691727442b77873eb3bf277f08b0
Sylvain Lichau <sylvain.lichau@etu.u-bordeaux.fr>
2021-01-10 14:32:55 +0100

    a lot of things improved

--- src/optim/flow/domain/algorithms/Dijkstra.java
+++ src/optim/flow/domain/algorithms/Dijkstra.java
@@ -1,6 +1,7 @@
 package optim.flow.domain.algorithms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -16,6 +17,8 @@ public class Dijkstra {
 	int nbVertices;
 	List<Double> dist;
 	List<Integer> predecessor;
+	List<Integer> VerticesTemporarlyLabeled;
+	double[] distances;
 	int start;
 	int end;
 
@@ -38,22 +41,70 @@ public class Dijkstra {
 	}
 
 	private int findMin(List<Integer> VerticesAvailable) {
-//<<<<<<< Updated upstream
-//		double min = Double.MAX_VALUE;
-//		int closestVertex = -1;
-//		for (int vertex : VerticesAvailable) {
-//			if (this.dist.get(vertex) < min) {
-//=======
 		double min = this.dist.get(VerticesAvailable.get(0));
 		int closestVertex = VerticesAvailable.get(0);
 		for(int vertex : VerticesAvailable) {
 			if(this.dist.get(vertex) < min) {
-//>>>>>>> Stashed changes
 				min = this.dist.get(vertex);
 				closestVertex = vertex;
 			}
 		}
 		return closestVertex;
+	}
+	
+	public List<Double> getPi(){
+		return this.dist;
+//		List<Double> pi = new ArrayList<Double>();
+////		double[] piArray = new double[this.residualNetwork.getNbVertices()];
+////		List<Integer> rest = new ArrayList<Integer>();
+////		rest.remove((Object)this.start);
+//		double toEndCost = 0;
+//		for(Edge edge:getShortestPathEdge()) {
+//			toEndCost+=edge.getReducedCost();
+//		}
+//		for (int i = 0; i < this.residualNetwork.getNbVertices(); i++) {
+//			if(this.VerticesTemporarlyLabeled.contains(i) || i == this.end)
+//				piArray[i] = toEndCost;
+//			else
+//				rest.add(i);
+//		}
+//		piArray[this.start] = 0;
+//		while(!rest.isEmpty()) {
+//			for(Integer i:rest) {
+//				if (!rest.contains(this.predecessor.get(i))) {
+//					double cost = -1;
+//					for(Edge edge:this.residualNetwork.getInEdges(i)) {
+//						if(edge.getSource() == this.predecessor.get(i) && cost == -1)
+//							cost = edge.getReducedCost();
+//						else if(edge.getSource() == this.predecessor.get(i) &&  edge.getReducedCost() < cost)
+//							cost = edge.getReducedCost();
+//						
+//					}
+//					piArray[i] = piArray[this.predecessor.get(i)] + cost;
+//					rest.remove((Object)i);
+//				}
+//			}
+//		}
+//		for (int i = 0; i < this.residualNetwork.getNbVertices(); i++) {
+//			pi.add(piArray[i]);
+//		}
+		
+//		for (int i = 0; i < this.residualNetwork.getNbVertices(); i++) {
+//			double cost = 0;
+//			if(this.VerticesTemporarlyLabeled.contains(i) || i == this.end)
+//				cost = toEndCost;
+//			else {
+//				for(Edge edge:getShortestPathEdgeTo(i)) {
+//					cost += edge.getReducedCost();
+//				}
+//			}
+//			pi.add(cost);
+//		}
+//		System.out.println(pi.equals(dist));
+//		System.out.println(pi.size()+" "+pi);
+//		System.out.println(dist.size()+" "+dist);
+//		System.out.println("");
+//		return pi;
 	}
 
 	private void updateDist(int v1, int v2) {
@@ -64,30 +115,18 @@ public class Dijkstra {
 				if( lightestNonFullEdge == null) {
 					lightestNonFullEdge = edge;
 				}
-				else if(edge.getCost() < lightestNonFullEdge.getCost())
+				else if(edge.getReducedCost() < lightestNonFullEdge.getReducedCost())
 					lightestNonFullEdge = edge;
 			}
 		}
 		
 		if (this.dist.get(v2) > this.dist.get(v1) + lightestNonFullEdge.getReducedCost()) {
+			
 			this.dist.set(v2, this.dist.get(v1) + lightestNonFullEdge.getReducedCost());
 			this.predecessor.set(v2, v1);
 		}
 	}
 
-	private List<Integer> getShortestPath() {
-		List<Integer> shortestPath = new ArrayList<Integer>();
-		if(this.predecessor.isEmpty()) {
-			return shortestPath;
-		}
-		int path = this.end;
-		while (this.predecessor.get(path) != -1) {
-			shortestPath.add(path);
-			path = this.predecessor.get(path);
-		}
-		shortestPath.add(path);
-		return shortestPath;
-	}
 	
 	private List<Edge> getShortestPathEdge() {
 		List<Edge> shortestPath = new ArrayList<Edge>();
@@ -113,14 +152,26 @@ public class Dijkstra {
 		return shortestPath;
 	}
 
+	//todo: stop when end is validate
 	public List<Edge> solve(Network network, int start, int end) {
 		initialize(network, start, end);
 		initializeLists();
+		
+//		tdistances = new double[this.residualNetwork.getNbVertices()];
+//		distances[this.start] = 0;
+		
 		List<Integer> VerticesAvailable = IntStream.rangeClosed(0, nbVertices - 1).boxed().collect(Collectors.toList());
 		while (!VerticesAvailable.isEmpty()) {
 			int vertex = findMin(VerticesAvailable);
 			if (VerticesAvailable.contains(vertex)) {
 				VerticesAvailable.remove((Object) vertex);
+			}
+			if(vertex == end) {
+				this.VerticesTemporarlyLabeled = VerticesAvailable;
+				for(Integer i:this.VerticesTemporarlyLabeled) {
+					this.dist.set(i, this.dist.get(this.end));
+				}
+				return getShortestPathEdge();
 			}
 			if (network.getOutEdges(vertex) != null) {
 				for (Edge outEdge : network.getOutEdges(vertex)) {
@@ -133,8 +184,61 @@ public class Dijkstra {
 						updateDist(vertex, outEdge.getDestination());
 				}
 			}
-
 		}
 		return getShortestPathEdge();
 	}
+	// used in capacityScaling algorithm.
+//	public List<Edge> solve(Network network, int start, boolean Delta) {
+//		initialize(network, start, end);
+//		initializeLists();
+//		List<Integer> VerticesAvailable = IntStream.rangeClosed(0, nbVertices - 1).boxed().collect(Collectors.toList());
+//		while (!VerticesAvailable.isEmpty()) {
+//			int vertex = findMin(VerticesAvailable);
+//			if (VerticesAvailable.contains(vertex)) {
+//				VerticesAvailable.remove((Object) vertex);
+//			}
+//			if(vertex == this.end) {
+//				System.out.println(VerticesTemporarlyLabeled);
+//				for(Integer i:this.VerticesTemporarlyLabeled) {
+//					this.dist.set(i, this.dist.get(this.end));
+//				}
+//				return getShortestPathEdge();
+//			}
+//			if (network.getOutEdges(vertex) != null) {
+//				for (Edge outEdge : network.getOutEdges(vertex)) {
+//					if(network.getClass().getName() == "optim.flow.domain.ResidualNetwork") {
+//						if( outEdge.getCapacity() - outEdge.getFlow() >0) {		
+//							updateDist(vertex, outEdge.getDestination());
+//						}
+//					}
+//					else
+//						updateDist(vertex, outEdge.getDestination());
+//				}
+//			}
+//
+//		}
+//		return getShortestPathEdge();
+//	}
+	
+	List<Edge> getShortestPathEdgeTo(int to){
+		int endSave = this.end;
+		this.end = to;
+		List<Edge> shor = getShortestPathEdge();
+		this.end = endSave;
+		return shor;
+	}
 }
+
+//private List<Integer> getShortestPath() {
+//	List<Integer> shortestPath = new ArrayList<Integer>();
+//	if(this.predecessor.isEmpty()) {
+//		return shortestPath;
+//	}
+//	int path = this.end;
+//	while (this.predecessor.get(path) != -1) {
+//		shortestPath.add(path);
+//		path = this.predecessor.get(path);
+//	}
+//	shortestPath.add(path);
+//	return shortestPath;
+//}
