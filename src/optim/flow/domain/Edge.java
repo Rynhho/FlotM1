9f0cf1e085b2d6c58c5137421347ede9d7b3a859
Sylvain Lichau <sylvain.lichau@etu.u-bordeaux.fr>
2021-01-14 17:56:42 +0100

    removed useless method

--- src/optim/flow/domain/Edge.java
+++ src/optim/flow/domain/Edge.java
@@ -66,7 +66,7 @@ public class Edge {
 			if(this.flow<0)
 				System.out.println(this + "flow: "+this.getFlow()+ " is residual? "+ this.isResidual());
 			else
-				System.out.println("opposite edge "+this.getOppositeEdge() + "flow: "+this.getOppositeEdge().getFlow()+ " is residual? "+ this.isResidual());
+			System.out.println("opposite edge "+this.getOppositeEdge() + "flow: "+this.getOppositeEdge().getFlow()+ " is residual? "+ this.isResidual());
 			throw new IllegalArgumentException("Flow must be not negative.\n");
 		}
 	}
@@ -81,10 +81,6 @@ public class Edge {
 	
 	public void updateReducedCost(double toAdd) {
 		this.reducedCost += toAdd;
-	}
-	
-	public void updateReducedCostScaling(double toAdd) {
-		this.reducedCost = this.reducedCost + toAdd;
 	}
 	
 	public int getSource() {
